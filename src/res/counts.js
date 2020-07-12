9a0515ebfbb04c342c4f68a843aa7ac14e6381b6
Ashish Vikram Kale <42481773+Ashishh-07@users.noreply.github.com>
2020-07-12 18:05:23 +0530

    Update counts.js

--- src/res/counts.js
+++ src/res/counts.js
@@ -7,12 +7,11 @@ function getCovidStats() {
 	.then(function(resp) { return resp.json() })
 	.then(function(data) {
 		let population = data.location.country_population;
-		let update = data.location.last_updated;
 		let confirmedCases = data.location.latest.confirmed;
 		let deaths = data.location.latest.deaths;
 
 		document.getElementById('population').innerHTML = population.toLocaleString('en');
-		document.getElementById('update').innerHTML = update.substr(0, 10);
+		
 		document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
 		document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
 		document.getElementById('percent').innerHTML = ((Number(deaths)/Number(confirmedCases))*100).toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2}) + "%";
