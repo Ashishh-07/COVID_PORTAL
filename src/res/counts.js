18ac66a91309ae2a440925f7b894b1c7f69e94b2
Ashish Vikram Kale <42481773+Ashishh-07@users.noreply.github.com>
2020-07-13 16:15:52 +0530

    Update counts.js

--- src/res/counts.js
+++ src/res/counts.js
@@ -3,20 +3,23 @@ window.onload = function() {
 }
 
 function getCovidStats() {
-	fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations/131?source=jhu&timelines=true")
+	fetch("https://api.covid19india.org/data.json")
 	.then(function(resp) { return resp.json() })
 	.then(function(data) {
-		let population = data.location.country_population;
-		let confirmedCases = data.location.latest.confirmed;
-		let deaths = data.location.latest.deaths;
-
-		document.getElementById('population').innerHTML = population.toLocaleString('en');
-		
+		let active = data.statewise[0].active;
+		let confirmedCases = data.statewise[0].confirmed;
+		let deaths = data.statewise[0].deaths;
+		let recovered = data.statewise[0].recovered;
+		let new_cases = data.statewise[0].deltaconfirmed;
+		let new_recovered = data.statewise[0].deltarecovered;
+		let new_deaths = data.statewise[0].deltadeaths;
 		document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
+		document.getElementById('active').innerHTML = active.toLocaleString('en');
 		document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
-		document.getElementById('percent').innerHTML = ((Number(deaths)/Number(confirmedCases))*100).toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2}) + "%";
-
-
+		document.getElementById('recover').innerHTML = recovered.toLocaleString('en');
+		document.getElementById('new_cases').innerHTML = "+"+new_cases.toLocaleString('en');
+		document.getElementById('new_recover').innerHTML = "+"+new_recovered.toLocaleString('en');
+		document.getElementById('new_deaths').innerHTML = "+"+new_deaths.toLocaleString('en');
 
 
 	})
